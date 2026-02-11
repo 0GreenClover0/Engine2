5cf6e77888e08bdfe6dc95474153a258067dea5a
Mikolaj Przybylski <clovers02123@gmail.com>
2026-02-11 23:48:55 +0100

    EHT: Add CUSTOM_EDITOR, CUSTOM_EDITOR_ONLY attributes

--- src/Serialization.h
+++ src/Serialization.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #ifndef NON_SERIALIZED
-
 #define NON_SERIALIZED
+#endif
 
+#ifndef CUSTOM_EDITOR
+#define CUSTOM_EDITOR
+#endif
+
+#ifndef CUSTOM_EDITOR_ONLY
+#define CUSTOM_EDITOR_ONLY
 #endif
