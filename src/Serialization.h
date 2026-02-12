05d8d843e51466472835fd8856ba0ae46708f468
Mikolaj Przybylski <clovers02123@gmail.com>
2026-02-12 01:15:48 +0100

    Serialization: Add descriptions to our attributes

--- src/Serialization.h
+++ src/Serialization.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #ifndef NON_SERIALIZED
+// Add this before a variable to tell EHT to not generate serialization code for it.
+// Add this before a class to tell EHT to not generaty any serialization code for the entire class.
 #define NON_SERIALIZED
 #endif
 
 #ifndef CUSTOM_EDITOR
+// Add this before a variable to tell EHT to not generate any editor code for it.
 #define CUSTOM_EDITOR
 #endif
 
 #ifndef CUSTOM_EDITOR_ONLY
+// Add this before a class to tell EHT to not generaty any editor code for the entire class,
+// with the exception of the draw_editor() function itself and a call to the custom editor.
 #define CUSTOM_EDITOR_ONLY
 #endif
